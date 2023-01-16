43108012087fa41b0aa4a7bfd5ecc30d552fee6a
vuralevren <evren@altogic.com>
2023-01-16 09:36:44 +0300

    persist removed

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
-import storage from "redux-persist/lib/storage";
 import { createWrapper } from "next-redux-wrapper";
 import rootSaga from "./rootSaga";
 
